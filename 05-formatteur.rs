33bb7c69a8d37b8ceb03b2ba092c075167aee34c
Axel Viala <axel.viala@darnuria.eu>
2019-05-23 19:59:01 +0200

    Légéres corrections et précisions

--- 05-formatteur.rs
+++ 05-formatteur.rs
@@ -5,40 +5,48 @@
 //
 // formatteur.rs
 //
-// On a vu dans les exercices précédents
-// qu'il est possible d'afficher le contenu
-// d'une variable ou d'une référence grâce à la
-// macro println! et du formatteur {}
+// On a vu dans les exercices précédents qu'il est possible d'afficher dans
+// le terminal la valeur d'une variable ou la valeur pointé par une
+// référence grace à la macro `println!` et du formateur par defaut `{}`.
+//
 // Nous allons ici explorer quelques autres formatteur
 
 fn main() {
 
-    let entier = 12;
+    let entier = 12; // La valeur de entier est 12
     let reference = &entier;
-    // Ici, on peut voir que le formatteur {} est capable
-    // d'identifier que la variable reference contient une adresse
-    // et que ce que l'on veut afficher doit être déréférencé.
-    println!("voici un entier: {} et une reference vers ceui-c: {}.", entier, reference);
+    //  ↑↑ La valeur de reference est une adresse.
+
+    //                      Ici `{}` insert directement `entier`
+    //                      ↓↓
+    println!("Voici entier: {} et une reference vers celui-çi: {}.",
+             entier, reference); //                            ↑↑
+                                 // Ici le formateur auto-déréférence
+                                 // `reference` vers `entier`
+
 
     // Pour ne pas déréférencer la variable mais afficher l'adresse
     // vers laquelle elle point on peut utiliser le formatteur {:p}
     println!("Notre entier est à l'adresse:  {:p}", reference);
+    //                                        ↑↑
+    // Si souhaite afficher l'adresse de notre référence
+    // On peut utiliser le formateur `{:p}`
 
-    // Déclarons un tableau:
+    // Déclarons un vecteur:
     // Ceux-ci se déclarent grâce à la macro vect![]
-    let tableau = vec![1,2,3];
+    let vector = vec![1,2,3];
 
-    // Si on essaye d'en afficher le contenus grâce au formatteur {}
+    // Si on essaye d'en afficher le contenus grâce au formatteur `{}`
     // On obtien une erreur de compilation:
-    println!("Voici le tableau: {}", tableau);
+    println!("Voici le vector: {}", vector);
     //                          ^
-    //                          | Ce formatteur ne convient pas pour un vect!
-    //                          | Erreur de compilation
+    //                          | Ce formatteur ne convient pas pour un `vec`
 
-    // A la place, il nous faut utiliser un formatteur de débug,
+    // A la place, il nous faut utiliser un formatteur de debug,
     // tel que {:?} ou {:#?}
-    println!("Voici le tableau: {:#?}", tableau);
+    println!("Voici le vector: {:#?}", vector);
 }
+
 
 // Il existe en rust bien d'autres formatteur, permettant de choisir
 // le nombre de décimal à afficher ou bien d'afficher en octal / binaire...
