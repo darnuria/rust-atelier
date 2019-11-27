4c363dddcc20c4cd775d8346a8867fb62bb03ab5
Axel Viala <axel.viala@darnuria.eu>
2019-11-27 15:57:10 +0100

    fix options

--- 09-options.rs
+++ 09-options.rs
@@ -11,20 +11,17 @@
 //
 // - Rappels sur les structures
 // - Type Option
+// - Manipulation avec match basique
 //
 // /!\ Quand vous verrez les symboles: `???`, il s'agit de code à
 // completer soit-même c'est normal que Rust indique une erreur! :)
 // -----------------------------------------------------------------------------
 
-// Étapes:
-//
-// 0. On complête la structure Point2D pour la faire compiler
-// 1. On ajoute une fonction `new` pour creer un Point2D
-// 2. On associe la fonction `new` avec un bloc `impl Point2D {}`
-// 2. On crée une fonction `add` qui fait l'addition de 2 `Points2D`, `self` et  `p`
-// 2.0 Observer ce qui se passe si on tente de réutiliser un des deux arguments de `add`!
-// 2.1 On reprend `add` pour prendre par référence `self` et `p`
-// 2.2 Écrire quelques tests pour nos fonctions
+// Etapes:
+// 
+// 1. Faire compiler le code ;)
+// 2. S'inspirer de l'exo 08 pour construire un user depuis une saisie clavier
+//   depuis la stdin
 
 /// Structure pour representer un utilisateur et optionnellement le nom de son animal.
 struct User {
@@ -36,35 +33,24 @@ struct User {
 // TODO Corriger la ligne çi dessous
 impl ??? {
 
-/// Construct a User with a optional pet.
-/// Take by move for simplicity.
-fn new(???) -> ??? { // <- Fix me
-    ???
-}
+    /// Construct a User with a optional pet.
+    /// Take by move for simplicity.
+    fn new(firstname: String, lastname: String, petname: Option<???>) -> User { // <- Fix me
+        User { ???, lastname, ??? }
+    }
 }
 
 fn main() {
     // Alyx form half-life games
-    let alyx = User::new("Alyx", "Vance", "Dog");
+    let alyx = User::new("Alyx".to_string(), "Vance".to_string(), Some("Dog".to_string()));
+    //                    ⬆
+    //                    `to_string()` sert a transformer une &str
+    //                    en String on vera plus tard pourquoi
 
     let petname = match alyx.petname {
-        ??? => ", I don't own a pet",
-        Some(???) => format!(", my pet name is: {}", ???)
-    }
+        None => ", I don't own a pet".to_string(),
+        Some(???) => format!(", my pet name is: {}", ???),
+    };
 
     println!("Hi I am {} {}{}.", alyx.firstname, alyx.lastname, petname);
 }
-
-// Pour commencer à écrire les tests il faudra décomenter le bloc ci-dessous:
-/*
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_new() {
-        let a = Point::new(1, 2);
-        let b = Point::new(3, 4);
-    }
-}
-*/
