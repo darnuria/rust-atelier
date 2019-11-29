a68acc414edc008c0ab39de2460ff5e9a72c7b66
Axel Viala <axel.viala@darnuria.eu>
2019-11-29 07:19:41 +0100

    Exercice about basic lifetimes.

---
+++ 10-lifetime-basics.rs
@@ -0,0 +1,36 @@
+// -----------------------------------------------------------------------------
+// Atelier Rust
+// 2019-11-17 - 14h -> 16h
+// Animateur: Axel Viala (darnuria)
+//
+// 10-lifetimes-basic.rs
+//
+// Objectifs pédagogiques:
+//
+// - Découverte du concept de lifetime
+//
+// Inspiré par l'exemple de *programming rust* de Jim Blandy et
+// Jason Orendorff.
+//
+// /!\ Quand vous verrez les symboles: `???`, il s'agit de code à
+// completer soit-même c'est normal que Rust indique une erreur! :)
+// -----------------------------------------------------------------------------
+
+// Etapes:
+// 
+// 1. Observer l'erreur de compilation
+// 2. Corriger autant que neccessaire le code pour faire compiler
+// le programme.
+
+fn main() {
+    // `r` ici sera une référence sur x.
+    let r;
+    {
+        // `x` est une valeur dans la pile
+        let x = 1;
+        r = &x;
+    } // <- fin de vie de `x`; après x est `dropped`
+    let _is_equal = *r == 1;
+    // Rust vérifie à la compilation la validité
+    // de la référence `r`.
+}
