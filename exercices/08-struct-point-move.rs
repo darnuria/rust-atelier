78dcdeaac7a1d11fa73650cc45531306c03f5f01
Hugo Trentesaux <hugo@trentesaux.fr>
2022-11-19 17:40:52 +0100

    Doc 08-struct-point-move

--- exercices/08-struct-point-move.rs
+++ exercices/08-struct-point-move.rs
@@ -5,8 +5,8 @@
 //
 // struct.rs
 //
-// Dans cet exercice on va écrire notre première structure et ses methodes,
-// fonctions associées, dans un second temps on vera comment faire du passage
+// Dans cet exercice on va écrire notre première structure et ses méthodes,
+// fonctions associées, dans un second temps on verra comment faire du passage
 // par référence
 //
 // Objectifs pédagogiques:
@@ -14,7 +14,7 @@
 // - Découverte des structures
 // - Découverte de la syntaxe pour faire des méthodes
 // - self
-// - passage par référence(borrowing), copie et semantiques de mouvement (move)
+// - passage par référence(borrowing), copie et sémantiques de mouvement (move)
 //
 // /!\ Quand vous verrez les symboles: `???`, il s'agit de code à
 // compléter soi-même c'est normal que Rust indique une erreur! :)
@@ -22,11 +22,11 @@
 
 // Étapes:
 //
-// 0. On complête la structure Point2D pour la faire compiler
-// 1. On ajoute une fonction `new` pour creer un Point2D
+// 0. On complète la structure Point2D pour la faire compiler
+// 1. On ajoute une fonction `new` pour créer un Point2D
 // 2. On associe la fonction `new` avec un bloc `impl Point2D {}`
-// 2. On crée une fonction `add` qui fait l'addition de 2 `Points2D`, `self` et  `p`
-// 2.0 Observer ce qui se passe si on tente de réutiliser un des deux arguments de `add`!
+// 2. On crée une fonction `add` qui fait l'addition de 2 `Points2D`, `self` et `p`
+// 2.0 Observer ce qui se passe si on tente de réutiliser un des deux arguments de `add` !
 // 2.1 On reprend `add` pour prendre par référence `self` et `p`
 // 2.2 Écrire quelques tests pour nos fonctions
 
@@ -36,7 +36,7 @@ struct Point2D {
     y: i32,
 }
 // ⬆
-// struct defini un reccord/enregistrement un pack de types
+// struct définit un record/enregistrement un pack de types
 
 impl Point2D { // Le type Self vaut le type après `impl`
     
@@ -58,7 +58,7 @@ impl Point2D { // Le type Self vaut le type après `impl`
         unimplemented!("A vous de jouer!")
     }
 
-    /// Offre une representation textuelle pour un humain d'un point
+    /// Offre une représentation textuelle pour un humain d'un point
     /// `Point2D { x: 3, y: 5 }
     fn show(self) -> String {
         unimplemented!("Whops pas encore implementé!")
@@ -75,7 +75,7 @@ impl Point2D { // Le type Self vaut le type après `impl`
 }
 
 fn main() {}
-// Pour commencer à écrire les tests il faudra décomenter le bloc ci-dessous:
+// Pour commencer à écrire les tests il faudra dé-commenter le bloc ci-dessous :
 #[cfg(test)]
 mod tests {
     use super::*;
